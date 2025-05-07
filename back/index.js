661bad79d2a3c4ec432e320e1a715e2cad93ecb7
João P. Carvalho <164025640+joao-the-carvalho@users.noreply.github.com>
2025-05-06 22:09:15 -0300

    Add files via upload

--- back/index.js
+++ back/index.js
@@ -4,6 +4,8 @@ const dotenv = require('dotenv')
 const sequelize = require('./config/database')
 const swaggerUi = require('swagger-ui-express')
 const swaggerDoc = require('./swagger/swagger.json')
+const path = require('path')
+const fs = require('fs')
 
 dotenv.config()
 
@@ -18,6 +20,35 @@ app.use('/api', require('./routes/marca'))
 
 // swagger
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc))
+
+
+app.use(express.static(path.join(__dirname, '../front')));
+
+
+const frontDir = path.join(__dirname, '../front')
+
+// Rota para o index.html
+app.get('/list', (req, res) => {
+  fs.readdir(frontDir, (err, files) => {
+    if (err) {
+      return res.status(500).send('Erro ao ler o diretório.')
+    }
+
+    res.send(`
+      <h1>Conteúdo da pasta "front"</h1>
+      <ul>
+        ${files.map(file => `<li><a href="/${file}">${file}</a></li>`).join('')}
+      </ul>
+    `)
+  })
+})
+
+app.use(express.static(frontDir))
+
+// Rota para o arquivo index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(frontDir, '/headerfooter/index.htm'))
+})
 
 // iniciar servidor
 const PORT = process.env.PORT || 3000
